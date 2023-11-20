11eb3272f2b6922c10ac67e9fe3900a12d4eeef3
Luke4910 <Jan210325@spengergasse.at>
2023-11-20 21:48:50 +0100

    1. Ein-aus Klappen geht. 2. Footer ist da. 3. Paragraphen sind in verkehrter reinfolge (mit css gemacht) 4. Laut w3c validator stimmt bei meinem footer etwas nicht, ansonsten geht es , css datei past laut w3c

---
+++ 2023-11__Auflösung PLF/script.js
@@ -0,0 +1,32 @@
+const überschriftenArtikel = document.querySelectorAll("div.news-articles > article > h2");
+
+überschriftenArtikel.forEach((überschrift) => {
+    überschrift.addEventListener("click", function(ereignis) {
+        const angeklickteÜberschrift = ereignis.target;
+        const artikelBehälter = angeklickteÜberschrift.parentElement;
+        const kindElemente = artikelBehälter.children;
+        const artikelInhalt = kindElemente[0];
+        artikelInhalt.istErweitert = !artikelInhalt.istErweitert;
+
+        const sichtbarkeit = artikelInhalt.istErweitert ? "visible" : "hidden";
+        const position = artikelInhalt.istErweitert ? "static" : "absolute";
+        const hintergrundFarbe = artikelInhalt.istErweitert ? "lightgray" : "";
+        const schriftFarbe = artikelInhalt.istErweitert ? "black" : "";
+
+        for (let i = 1; i < kindElemente.length; i++) {
+            kindElemente[i].style.visibility = sichtbarkeit;
+            kindElemente[i].style.position = position;
+        }
+
+        artikelBehälter.style.backgroundColor = hintergrundFarbe;
+        artikelBehälter.style.color = schriftFarbe;
+    });
+});
+const paragraphContainer = document.getElementById('news-articles');
+const paragraphs = paragraphContainer.querySelectorAll('p');
+
+for (let i = paragraphs.length - 1; i >= 0; i--) {
+  const paragraph = paragraphs[i];
+  paragraphContainer.removeChild(paragraph); 
+  paragraphContainer.appendChild(paragraph); 
+}
